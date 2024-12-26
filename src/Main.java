de8509233b4e4449c8fb27f605ca57d78f68cc10
Nikita Rubtsov <118419926+kad1rka@users.noreply.github.com>
2024-12-26 10:40:49 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) throws ParseException {
         System.out.println(CountNumebrs("01234safd"));
     }
+
     public static int CountNumebrs(String string) {
         int count = 0;
-        for (int i = 0; i <  string.length(); i++) {
-            if (Character.isDigit(string.charAt(i))) count++;
+        for (char c : string.toCharArray()) {
+            if (Character.isDigit(c)) count++;
         }
         return count;
     }
